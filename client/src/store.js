c992544ac812fe51a4cc33d931fad5f17bd963d1
Riya Patel <r392pate@uwaterloo.ca>
2022-02-26 13:15:22 -0500

    updated store.js file

--- client/src/store.js
+++ client/src/store.js
@@ -6,18 +6,21 @@ import { getAllVendorReducer } from "./reducers/vendorReducer";
 import { cartReducer } from "./reducers/cartReducer";
 import { loginUserReducer, registerUserReducer } from "./reducers/userReducer";
 
-
 const finalReducer = combineReducers({
     getAllVendorReducer: getAllVendorReducer,
     cartReducer: cartReducer,
     registerUserReducer: registerUserReducer,
     loginUserReducer: loginUserReducer
 });
-const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
 
+const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+const currentUser = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : null;
 const initialState = {
     cartReducer: {
         cartItems: cartItems
+    },
+    loginUserReducer:{
+        currentUser: currentUser
     }
 };
 const composeEnhancer = composeWithDevTools({});
