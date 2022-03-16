b036849eaa8561da2c3b2475ac61c23af52dee05
Divya NA <d3na@uwaterloo.ca>
2022-03-16 12:20:13 -0400

    updated vendor route

--- routes/vendorRoute.js
+++ routes/vendorRoute.js
@@ -11,4 +11,28 @@ router.get('/getallvendors', async (req, res) => {
     }
 });
 
+router.post('/addpackage', async (req, res) => {
+    try {
+        
+        const addpackage = req.body.addpackage
+        
+        const oldPackage = await Vendor.findOne({'name': addpackage.uname})
+        const newPackage = oldPackage
+        const k1 = addpackage.cusine_type
+        const pr = oldPackage.prices[0]
+        pr[k1] = addpackage.prices
+        
+        newPackage.name = oldPackage.name    
+        newPackage.cusine_type =  oldPackage.cusine_type.concat(addpackage.cusine_type),
+        newPackage.prices =  pr,
+        newPackage.category = addpackage.category,
+        newPackage.image =  oldPackage.image
+       
+        await newPackage.save()
+        res.send('New Pizza Added Successfully');  
+    } catch (error) {
+        return res.status(400).json({ message: error });
+    }
+});
+
 module.exports = router;
