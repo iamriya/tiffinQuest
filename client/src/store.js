c15e98b3bafdfeb2e7be14213cd130fe6a8b6a0e
Divya NA <d3na@uwaterloo.ca>
2022-03-24 11:21:13 -0400

    updated store.js file

--- client/src/store.js
+++ client/src/store.js
@@ -2,15 +2,21 @@ import { combineReducers } from "redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { getAllVendorReducer } from "./reducers/vendorReducer";
+import { getAllVendorReducer, addPackageReducer } from "./reducers/vendorReducer";
 import { cartReducer } from "./reducers/cartReducer";
 import { loginUserReducer, registerUserReducer } from "./reducers/userReducer";
+import {getUserOrderReducer, placeOrderReducer, getAllOrderReducer } from "./reducers/orderReducer";
+
 
 const finalReducer = combineReducers({
     getAllVendorReducer: getAllVendorReducer,
     cartReducer: cartReducer,
     registerUserReducer: registerUserReducer,
-    loginUserReducer: loginUserReducer
+    loginUserReducer: loginUserReducer,
+    placeOrderReducer: placeOrderReducer,
+    getUserOrderReducer: getUserOrderReducer,
+    addPackageReducer: addPackageReducer,
+     getAllOrderReducer: getAllOrderReducer
 });
 
 const cartItems = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
