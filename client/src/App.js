eb29422ed4351dffb1235321ea7086e545a07437
Riya Patel <r392pate@uwaterloo.ca>
2022-03-27 11:21:13 -0400

    updated app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
+import Cartscreen from './screens/Cartscreen';
+import Loginscreen from './screens/Loginscreen';
+import Registrationscreen from './screens/Registrationscreen';
+import Ordersscreen from './screens/Ordersscreen';
+import Adminscreen from './screens/Adminscreen';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Homescreen />
+      {/* <Homescreen></Homescreen> */}
+      <BrowserRouter>
+        <Route path="/" exact component={Homescreen} />
+        <Route path="/cart" exact component={Cartscreen} />
+        <Route path="/login" exact component={Loginscreen} />
+        <Route path='/register' exact component={Registrationscreen} />
+        <Route path='/orders' exact component={Ordersscreen} />
+        <Route path='/admin' component={Adminscreen} />
+      </BrowserRouter> 
     </div>
   );
 }
